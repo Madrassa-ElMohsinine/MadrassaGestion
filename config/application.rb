ec4b1e4edfab245c1289cf008aacd0f058b17c23
Madrassa-ElMohsinine <madrassaelmohsinine@gmail.com>
2025-05-21 21:37:03 +0200

    Fix: load Rails properly for Render deployment

--- config/application.rb
+++ config/application.rb
@@ -1,1 +1,11 @@
+require_relative "boot"
+require "rails/all"
+
+Bundler.require(*Rails.groups)
+
+module MadrassaFedena
+  class Application < Rails::Application
+    config.load_defaults 7.0
+  end
+end
 # Placeholder for Rails application.rb
